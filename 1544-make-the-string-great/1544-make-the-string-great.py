53c45903e553791125370a7d5e20231e15453df7
VAMSHI KUNA <118479150+kunavamshi@users.noreply.github.com>
2024-04-05 12:11:39 +0530

    Time: 16 ms (61.14%), Space: 11.7 MB (29.38%) - LeetHub

---
+++ 1544-make-the-string-great/1544-make-the-string-great.py
@@ -0,0 +1,9 @@
+class Solution(object):
+    def makeGood(self, s):
+        stack = []
+        for char in s:
+            if stack and abs(ord(char) - ord(stack[-1])) == 32:
+                stack.pop()
+            else:
+                stack.append(char)
+        return ''.join(stack)
